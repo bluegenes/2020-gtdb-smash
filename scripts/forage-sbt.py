d39bf3989b7d2be420fa22be77c765cbfa30e5a7
N Tessa Pierce <ntpierce@gmail.com>
2020-05-21 19:21:36 -0700

    grow and forage working on test data

--- scripts/forage-sbt.py
+++ scripts/forage-sbt.py
@@ -8,12 +8,14 @@ import pandas as pd
 
 import screed
 import sourmash
-
+# why fig doesn't work without this? best practice?
+import sourmash.fig
 
 def load_sbt_file(tree_file):
     if os.path.exists(tree_file):
         try:
             sbt = sourmash.load_sbt_index(tree_file)
+            sys.stderr.write(f"loaded sbt file at {tree_file}")
             return sbt
         except:
             sys.stderr.write(f"cannot load sbt file at {tree_file}\n")
@@ -34,21 +36,28 @@ def forage_by_name(sbt, query_accessions, threshold=0.1):
     return acc2sig
 
 
-def assess_group_distance(groupD, acc2sig): # provide options for choosing to use abund or not? (compute either one OR both, as is done now)
+def sourmash_compare_and_plot(siglist, ignore_abundance=False, plot_and_save=False, filename=None):
+    dist_matrix = sourmash.compare.compare_all_pairs(siglist, ignore_abundance=ignore_abundance)
+    labels = [item.name() for item in siglist]
+    #optionally plot with sourmash plot
+    if plot_and_save:
+        dist_fig = sourmash.fig.plot_composite_matrix(dist_matrix, labels)
+        if filename:
+            plt.savefig(filename) #, format="svg")
+    return dist_matrix, labels
+
+
+def assess_group_distance(groupD, acc2sig, abund=False): # provide options for choosing to use abund or not? (compute either one OR both, as is done now)
     for group, acclist in groupD.items():
         siglist = [acc2sig[acc] for acc in acclist] # get signatures
-        compare_and_plot(siglist, ignore_abundance=False, filename= f"{group}_cosine") # need ksize, scaled, etc. can we pass in a base name, then add group name?
-        compare_and_plot(siglist, ignore_abundance=True, filename= f"{group}_jaccard")
-        # do we want to save distances as a csv for later? kinda depends on how long it takes to regenerate this...
-        # do we want to compute all-by-all at the end? or just care about the groups?
+        jaccard_dist, labels=sourmash_compare_and_plot(siglist, ignore_abundance=False) #, filename= f"{group}_jaccard.svg") # need ksize, scaled, etc. can we pass in a base name, then add group name?
 
+        ## TO DO ##
+        # build distance:: steps_to_common_ancestor!
+        # use groupDF ("species", "genus", etc info)
 
-def compare_and_plot(siglist, ignore_abundance=False, filename=None):
-    dist_matrix = sourmash.compare.compare_all_pairs(siglist, ignore_abundance=ignore_abundance)
-    plot_mat = sourmash.fig.load_matrix_and_labels(dist_matrix)
-    dist_fig = sourmash.fig.plot_composite_matrix(plot_mat)
-    if filename:
-        plt.savefig(filename) #, format="svg")
+        if abund:
+            cosine_dist, labels=sourmash_compare_and_plot(siglist, ignore_abundance=True)  #, filename= f"{group}_cosine.svg")
 
 
 def csv_reader(groups_file):
@@ -64,7 +73,7 @@ def csv_reader(groups_file):
             print(e)
     elif '.xls' in samples_file:
         try:
-            samples = pd.read_excel(groupss_file, dtype=str, sep=separator)
+            samples = pd.read_excel(groups_file, dtype=str, sep=separator)
         except Exception as e:
             sys.stderr.write(f"\n\tError: {groups_file} file is not properly formatted. Please fix.\n\n")
             print(e)
@@ -72,16 +81,22 @@ def csv_reader(groups_file):
 
 
 
-def build_sbt_accession_from_filename(groupInfo, db_infofile, filename=None):
-    import pdb; pdb.set_trace()
+def match_sbt_accession(groupInfo, db_infofile, filename=None):
     # if dataframe doesn't have filename
     if "filename" not in groupInfo.columns:
         dbInfo = csv_reader(db_infofile)
+        if "accession" in dbInfo.columns:
         # add filenames to groupInfo
-        groupInfo=groupInfo.merge(dbInfo[["accession", "filename"]], on=["accession"])
-    #generate sbt_accession from filenames
-    samples["sbt_accession"] = samples["filename"].str.rsplit("_", 1, expand=True)[0]
-    #sbt_accession = os.path.basename(fasta_file.rsplit("_", 1)[0])
+            groupInfo=groupInfo.merge(dbInfo[["accession", "filename"]], on=["accession"])
+           #generate sbt_accession from filenames
+            groupInfo["sbt_accession"] = groupInfo["filename"].str.rsplit("_", 1, expand=True)[0]
+        elif "sbt_accession" in dbInfo.columns:
+            dbInfo["acc2"] = dbInfo["sbt_accession"].str.split("_", 1, expand=True)[1] #remove leading GB_ or RS_
+            dbInfo["acc2"] = dbInfo["acc2"].str.rsplit(".", 1, expand=True)[0] #remove trailing .1
+            groupInfo=groupInfo.merge(dbInfo[["acc2", "sbt_accession"]], left_on=["accession"], right_on=["acc2"])
+            groupInfo.drop(["acc2"], axis="columns")
+    else:
+        groupInfo["sbt_accession"] = groupInfo["filename"].str.rsplit("_", 1, expand=True)[0]
     if filename:
         if filename.endswith(".csv"):
             groupInfo.to_csv(filename)
@@ -92,18 +107,23 @@ def build_sbt_accession_from_filename(groupInfo, db_infofile, filename=None):
     return groupInfo
 
 
-def build_group2accession(group_infofile, db_infofile="gtdb-lineages.csv"):
+def build_group_to_accession(group_infofile, db_infofile=None):
     groupDF = csv_reader(group_infofile)
 
     if "sbt_accession" not in groupDF.columns:
+        if not db_infofile:
+            sys.stderr.write(f"your query csv does not have the 'sbt_accession' column. " \
+            "We can autogenerate if you provide an approprate --database_csv file " \
+            "containing the names of hashes in the sbt that will be searched\n")
+            sys.exit()
+
         updated_groupInfofile = group_infofile.rsplit(".", 1)[0] + ".sbtinfo.csv"
-        groupDF = find_sbt_accessions(groupDF, db_infofile, updated_groupInfofile)
+        groupDF = match_sbt_accession(groupDF, db_infofile, updated_groupInfofile)
 
     # build group: accession dictionary
-    group2acc = groupDf.groupby('path').agg({'sbt_accession':lambda x: list(x)})
+    group2acc = groupDF.groupby('path').agg({'sbt_accession':lambda x: list(x)}).to_dict()["sbt_accession"]
     # build set of unique query accessions
     acc_set = set(groupDF["sbt_accession"].unique())
-
     return group2acc, acc_set
 
 
@@ -118,7 +138,7 @@ if __name__ == "__main__":
     p = argparse.ArgumentParser()
     p.add_argument("sbt")
     p.add_argument("--query_csv", required=True)
-    p.add_argument("--database_csv", required=True)
+    p.add_argument("--database_csv")
     p.add_argument("--threshold", type=int, default=0.1)
     args = p.parse_args()
     sys.exit(main(args))
--- scripts/grow-sbtmh.py
+++ scripts/grow-sbtmh.py
@@ -18,7 +18,7 @@ def maybe_read_fasta_file(fasta_file):
         accession = os.path.basename(fasta_file.rsplit("_", 1)[0])
     except:
         # log the error somehow
-        sys.stderr.write(f"cannot read fasta file {fasta_file}\n")
+        sys.stderr.write(f"\ncannot read fasta file {fasta_file}\n")
     return records, accession
 
 
@@ -28,7 +28,7 @@ def maybe_load_sbt_file(tree_file):
         try:
             sbt = sourmash.load_sbt_index(tree_file)
         except:
-            sys.stderr.write(f"cannot load sbt file at {tree_file}\n")
+            sys.stderr.write(f"\ncannot load sbt file at {tree_file}\n")
     else:
         sbt = sourmash.create_sbt_index()
     return sbt
@@ -67,27 +67,46 @@ def load_or_generate_sig(input_file, ksize, scaled, alphabet, abundance):
     return sig
 
 
-def grow_sbt(input_files, sbt_file, ksize, scaled, alpha, abund, input_is_directory):
+def grow_sbt(input_files, sbt_file, ksize, scaled, alpha, abund, input_is_directory, dup_sigs, sig2file):
+    sig2filename={}
+    duplicated_md5_sigs=set()
+    md5sum_set=set()
     if input_is_directory:
         refdir = input_files[0]
         input_files = [os.path.join(refdir, f) for f in os.listdir(refdir)]
     # create or load sbt
     sbt = maybe_load_sbt_file(sbt_file)
     # iterate through input files; add to sbt
-    md5sum_set=set()
-    for inp in input_files:
-        #sys.stderr.write(f"{inp}\n")
-        sig = load_or_generate_sig(inp, ksize, scaled, alpha, abund)
-        if sig:
-            # protein and hp were exiting with ValueError: This will insert duplicated entries .. maybe checking for empty mins will solve?
-            if len(sig.minhash.get_mins()) > 0:
+    for n, filename in enumerate(input_files):
+        # swipe some handy progress reporting code from titus:
+        if n % 100 == 0:
+            sys.stderr.write(f"... loading {filename} file {n} of {len(input_files)}")
+        sig = load_or_generate_sig(filename, ksize, scaled, alpha, abund)
+        if sig: # is this necessary?
+            if sig.minhash:
+                sig2filename[sig.name()]=filename
                 md5 = sig.md5sum()
                 if md5 not in md5sum_set:
                     leaf = SigLeaf(md5, sig)
                     sbt.add_node(leaf)
                     md5sum_set.add(md5)
                 else:
+                    # this only records signatures that do not get added.
+                    # (original sig with that md5 is in the sbt)
+                    duplicated_md5_sigs.add(sig.name())  # don't really need correspondence, do we? just keep set.
                     sys.stderr.write(f"duplicated md5sum {md5}!")
+
+                    # this is kinda dumb. don't really want to track this if we can help it.
+                    sig2filenames[sig.name()]=sig.filename()
+
+    # keep namemap, becuase the filenames from gtdb-lineage-csv don't seem to be what we need
+    # name and filename are kinda redundant here. decide what we want to keep.
+    with open(sig2file, "w") as out:
+        out.write(",".join(["sbt_accession", "filename"]) + "\n")
+        for name, filename in sig2filename.items():
+            out.write(",".join([name, filename]) + "\n")
+    with open(dup_sigs, "w") as out:
+        out.write("\n".join(duplicated_md5_sigs))
     # save the tree
     # hmm.. overwriting seems complicated but managed? see sourmash/sbt_storage.py
     sbt.save(sbt_file)
@@ -103,4 +122,13 @@ if __name__ == "__main__":
     p.add_argument("--input-is-directory", action="store_true")
     p.add_argument("--track-abundance", action="store_true")
     args = p.parse_args()
-    sys.exit(grow_sbt(args.input_files, args.sbt, args.ksize, args.scaled, args.alphabet, args.track_abundance, args.input_is_directory))
+    if not args.sbt.endswith(".sbt.zip"):
+        sys.stderr.write("sbt file must end with .sbt.zip")
+        sys.exit()
+    else:
+        dupes= args.sbt.rsplit(".sbt.zip")[0] + ".md5_duplicates.txt"
+        print(dupes)
+        signame2filename= args.sbt.rsplit(".sbt.zip")[0] + ".signame2filenames.csv"
+        print(signame2filename)
+
+    sys.exit(grow_sbt(args.input_files, args.sbt, args.ksize, args.scaled, args.alphabet, args.track_abundance, args.input_is_directory, dupes, signame2filename))
